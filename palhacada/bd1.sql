fc0851b93b4e949841e071d99a34a2882e638e6f
vitorcruzeiro <vitor.cruzeiro@fc.unesp.br>
2018-06-18 16:16:42 -0300

    CREATE TABLE com constraints cascade

--- palhacada/bd1.sql
+++ palhacada/bd1.sql
@@ -3,8 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- Estrutura da tabela `celula`
-
+-- Tabelas entidades
 CREATE TABLE `celula` (
   `nome` varchar(255) NOT NULL,
   `rua` varchar(255) NOT NULL,
@@ -15,13 +14,10 @@ CREATE TABLE `celula` (
   `uf` varchar(2) NOT NULL,
   `cep` varchar(8) NOT NULL,
   `feira` varchar(10) NOT NULL,
+  `status` boolean NOT NULL DEFAULT TRUE,
   `n_membros` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`nome`,`cidade`,`uf`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
--- Estrutura da tabela `igreja`
 
 CREATE TABLE `igreja` (
   `nome` varchar(255) NOT NULL,
@@ -35,8 +31,6 @@ CREATE TABLE `igreja` (
   `n_membros` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Estrutura da tabela `membros`
 
 CREATE TABLE `membros` (
   `cpf` bigint(20) NOT NULL,
@@ -55,18 +49,12 @@ CREATE TABLE `membros` (
   PRIMARY KEY (`cpf`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Estrutura da tabela `rede`
-
 CREATE TABLE `rede` (
   `cod_rede` int(11) NOT NULL AUTO_INCREMENT,
   `cor` varchar(15) NOT NULL,
   PRIMARY KEY (`cod_rede`),
   UNIQUE (`cod_rede`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
--- Constraints for dumped tables
 
 -- RELAÇÕES:
 
@@ -75,7 +63,7 @@ CREATE TABLE `igreja_pastor` (
   `nome_igreja` varchar(255) NOT NULL,
   PRIMARY KEY (`cpf_pastor`, `nome_igreja`),
   CONSTRAINT `igreja_pastor_nome_igreja_FK` FOREIGN KEY (`nome_igreja`) REFERENCES `igreja` (`nome`),
-  CONSTRAINT `igreja_pastor_cpf_FK` FOREIGN KEY (`cpf_pastor`) REFERENCES `membros` (`cpf`)
+  CONSTRAINT `igreja_pastor_cpf_FK` FOREIGN KEY (`cpf_pastor`) REFERENCES `membros` (`cpf`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `membro_celula`
@@ -85,14 +73,14 @@ CREATE TABLE `membro_celula`
   `uf_celula` VARCHAR(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
   PRIMARY KEY (`cpf_membro`, `nome_celula`, `cidade_celula`, `uf_celula`),
   CONSTRAINT `celula_membro_FK` FOREIGN KEY (`nome_celula`, `cidade_celula`, `uf_celula`) REFERENCES `celula` (`nome`, `cidade`, `uf`),
-  CONSTRAINT `cpf_membro_celula_FK` FOREIGN KEY (`cpf_membro`) REFERENCES `membros`(`cpf`))
+  CONSTRAINT `cpf_membro_celula_FK` FOREIGN KEY (`cpf_membro`) REFERENCES `membros`(`cpf`) ON DELETE CASCADE)
   ENGINE = InnoDB;
 
 CREATE TABLE `membro_igreja` (
   `cpf_membro` bigint(20) NOT NULL,
   `nome_igreja` varchar(255) NOT NULL,
   PRIMARY KEY (`cpf_membro`, `nome_igreja`),
-  CONSTRAINT `membro_igreja_cpf_FK` FOREIGN KEY (`cpf_membro`) REFERENCES `membros`(`cpf`),
+  CONSTRAINT `membro_igreja_cpf_FK` FOREIGN KEY (`cpf_membro`) REFERENCES `membros`(`cpf`) ON DELETE CASCADE,
   CONSTRAINT `membro_igreja_nome_igreja_FK` FOREIGN KEY (`nome_igreja`) REFERENCES `igreja` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -114,7 +102,7 @@ CREATE TABLE `lider_rede` (
   `cod_rede` int NOT NULL, 
   PRIMARY KEY (`cpf_membro`, `cod_rede`),
   CONSTRAINT `lide_rede_cpf_FK` FOREIGN KEY (`cpf_membro`) REFERENCES `membros` (`cpf`),
-  CONSTRAINT `lide_rede_cod_FK` FOREIGN KEY (`cod_rede`) REFERENCES `rede` (`cod_rede`)
+  CONSTRAINT `lide_rede_cod_FK` FOREIGN KEY (`cod_rede`) REFERENCES `rede` (`cod_rede`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `rede_celula` (
@@ -151,7 +139,6 @@ CREATE TABLE `culto` (
 
 -- INSERÇÕES TESTE (MOCKUPS)
 -- Extraindo dados da tabela `igreja`
-
 INSERT INTO `igreja` (`nome`, `rua`, `numero`, `complemento`, `bairro`, `cidade`, `uf`, `cep`, `n_membros`) VALUES
 ('123', 'rua', '1', 'complemento', 'bairro', 'cidade', '0', '12345-12', 0),
 ('124as', 'rua', '1', 'complemento', 'bairro', 'cidade', '0', '12345-12', 0),
@@ -172,7 +159,6 @@ INSERT INTO `igreja` (`nome`, `rua`, `numero`, `complemento`, `bairro`, `cidade`
 ('TESTE_MEMBRO_IGREJA', 'rua', '1', 'complemento', 'bairro', 'cidade', '0', '12345-12', 0);
 
 -- Extraindo dados da tabela `membros`
-
 INSERT INTO `membros` (`cpf`, `nome`, `email`, `telefone`, `sexo`, `cidade`, `uf`, `cep`, `data_nasc`, `rua`, `numero`, `bairro`, `complemento`) VALUES
 (-413, 'AaE', 'aea@asde.com', '(18)99874-5784', '', 'cidade', '0', '12345-12', '0000-00-00', 'rua', '1', 'bairro', 'complemento'),
 (1, 'AaE', 'aea@asde.com', '(18)99874-5784', '', 'cidade', '0', '12345-12', '0000-00-00', 'rua', '1', 'bairro', 'complemento'),
@@ -193,9 +179,7 @@ INSERT INTO `membros` (`cpf`, `nome`, `email`, `telefone`, `sexo`, `cidade`, `uf
 (30057107158, 'AaE', 'aea@asde.com', '(18)99874-5784', '', 'cidade', '0', '12345-12', '0000-00-00', 'rua', '1', 'bairro', 'complemento'),
 (30057107258, 'AaE', 'aea@asde.com', '(18)99874-5784', '', 'cidade', '0', '12345-12', '0000-00-00', 'rua', '1', 'bairro', 'complemento');
 
--- --------------------------------------------------------
 -- Extraindo dados da tabela `igreja_pastor`
-
 INSERT INTO `igreja_pastor` (`cpf_pastor`, `nome_igreja`) VALUES
 (-413, 'igreja5555'),
 (124, 'TESTE_MEMBRO_IGREJA'),
@@ -208,10 +192,7 @@ INSERT INTO `igreja_pastor` (`cpf_pastor`, `nome_igreja`) VALUES
 (654564, 'TESTE_MEMBRO_IGREJA'),
 (57506574, '1577');
 
--- --------------------------------------------------------
-
 -- Extraindo dados da tabela `membro_igreja`
-
 INSERT INTO `membro_igreja` (`cpf_membro`, `nome_igreja`) VALUES
 (15977, 'TESTE_MEMBRO_IGREJA'),
 (124, 'TESTE_MEMBRO_IGREJA'),
