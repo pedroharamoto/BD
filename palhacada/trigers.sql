b42efe077a3d51f38c854f21daba3dc785d8b56a
vitorcruzeiro <vitor.cruzeiro@fc.unesp.br>
2018-06-18 15:26:45 -0300

    Triggers de cel e igre finalizados
    só copiar e colar

--- palhacada/trigers.sql
+++ palhacada/trigers.sql
@@ -1,16 +1,16 @@
---TABELA MEMBROS_IGREJA
+-- TABELA MEMBROS_IGREJA
 CREATE TRIGGER `contador_igreja_add`
 AFTER INSERT ON `membro_igreja`
 FOR EACH ROW
 	UPDATE igreja SET n_membros = n_membros +1
-	WHERE nome = NEW.nome_igreja
--------
+	WHERE nome = NEW.nome_igreja;
+
 CREATE TRIGGER `contador_igreja_remove`
 AFTER DELETE ON `membro_igreja`
 FOR EACH ROW 
 	UPDATE igreja SET n_membros = n_membros - 1
-	WHERE nome = OLD.nome_igreja
--------
+	WHERE nome = OLD.nome_igreja;
+
 DELIMITER $$
 CREATE TRIGGER `contador_igreja_update`
 AFTER UPDATE ON `membro_igreja`
@@ -23,26 +23,23 @@ BEGIN
 END $$
 DELIMITER ;
 
-------------------------------------------------------
-
---TABELA MEMBROS_IGREJA
+-- TABELA MEMBRO_CELULA
 CREATE TRIGGER `contador_celula_add`
 AFTER INSERT ON `membro_celula`
 FOR EACH ROW
 	UPDATE celula SET n_membros = n_membros +1
 	WHERE nome = NEW.nome_celula and 
 	uf = NEW.uf_celula and 
-	cidade = NEW.cidade_celula
+	cidade = NEW.cidade_celula;
 
--------
 CREATE TRIGGER `contador_celula_remove`
-AFTER INSERT ON `membro_celula`
+AFTER DELETE ON `membro_celula`
 FOR EACH ROW
 	UPDATE celula SET n_membros = n_membros - 1
 	WHERE nome = OLD.nome_celula and 
 	uf = OLD.uf_celula and 
-	cidade = OLD.cidade_celula
--------
+	cidade = OLD.cidade_celula;
+
 DELIMITER $$
 CREATE TRIGGER `contador_celula_update`
 AFTER UPDATE ON `membro_celula`
@@ -55,6 +52,6 @@ BEGIN
 	UPDATE celula SET n_membros = n_membros + 1
 	WHERE nome = NEW.nome_celula and 
 	uf = NEW.uf_celula and 
-	cidade = NEW.cidade_celula
+	cidade = NEW.cidade_celula;
 END $$
 DELIMITER ;
